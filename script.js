fe2ec25959ee0ab150a1330b26dd03b11c18e9c1
3mpty <aurelien.corre@outlook.fr>
2022-12-03 15:19:33 +0100

    The End...

--- script.js
+++ script.js
@@ -4,11 +4,33 @@ const border = document.querySelector(".border");
 const loader = document.querySelector(".loaderContainer");
 const game = document.querySelector(".gameContainer");
 
+// Timeline animation
+
+const ratio = 0.6;
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: ratio,
+};
+const handleIntersect = function (entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.intersectionRatio > ratio) {
+      entry.target.classList.add("reveal-visible");
+      observer.unobserve(entry.target);
+    }
+  });
+};
+const observer = new IntersectionObserver(handleIntersect, options);
+
+document.querySelectorAll(".reveal").forEach(function (appear) {
+  observer.observe(appear);
+});
+
 // Load event for name & border
 
 window.addEventListener("load", () => {
-  lastName.style.marginTop = "5rem";
-  firstName.style.marginTop = "0";
+  lastName.style.marginLeft = "5rem";
+  firstName.style.marginLeft = "15%";
   border.style.width = "75%";
 });
 
